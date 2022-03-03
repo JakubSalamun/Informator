1343fb0e303996a344915c9bff6741d7d2bdae05
Askalt <jakub.salamun98@gmail.com>
2022-03-03 22:30:31 +0100

    Add project files.

---
+++ Informator/Adress.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Informator
+{
+   public class Adress
+    {
+        public Adress(string city, string streat,string postalCode)
+        {
+            City = city;
+            Streat = streat;
+            PostalCode = postalCode;
+        }
+
+        public string City { get; set; }
+        public string Streat { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
---
+++ Informator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Informator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj podaj swoje dane!");
+            Console.Write("Imie:");
+            var userInputName = Console.ReadLine();
+            Console.Write("Nazwisko:");
+            var userInputLastName = Console.ReadLine();
+
+            Console.Write("Miasto:");
+            var userInputAdressCity= Console.ReadLine();
+
+            Console.Write("Ulica:");
+            var userInputAdressStret = Console.ReadLine();
+
+            Console.Write("Kod Ppocztowy:");
+            var userInputAdressPostalCode = Console.ReadLine();
+
+            PersonHelp(userInputName, userInputLastName, new Adress(userInputAdressCity, userInputAdressStret, userInputAdressPostalCode));
+
+        }
+
+
+        static void PersonHelp(string firstName,string lastName,Adress adress)
+        {
+            Person person = new Person();
+            person.FirstName = firstName;
+            person.LastName = lastName;
+            person.adress = adress;
+            
+
+            Console.WriteLine($"First name:{person.FirstName}, Last name:{person.LastName}, Lokalizacjia:  {adress.City} {adress.PostalCode} {adress.Streat}");
+        }
+    }
+}
